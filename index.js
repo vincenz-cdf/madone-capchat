a447729785072def0005fd3a9e8402d567b639df
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2023-06-23 10:37:37 +0200

    update users

--- index.js
+++ index.js
@@ -71,6 +71,26 @@ app.get('/currentUser', verifyToken, (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    const sqlCheck = 'SELECT id, username, email FROM user WHERE id != 1';
+    connection.query(sqlCheck, function (err, results) {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.post('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const username = req.body.username;
+    const email = req.body.email;
+
+    const sqlCheck = 'UPDATE user SET username = ?, email = ? where id = ?';
+    connection.query(sqlCheck, [username, email, id], function (err, results) {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
 
 app.post('/register', (req, res) => {
     const username = req.body.username;
